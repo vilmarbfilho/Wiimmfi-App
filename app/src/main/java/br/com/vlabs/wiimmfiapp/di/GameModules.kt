e31941944404087bb9a27cf1f2179b8ddd549f93
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2020-09-05 15:20:22 -0300

    Setup repository in DI module

--- app/src/main/java/br/com/vlabs/wiimmfiapp/di/GameModules.kt
+++ app/src/main/java/br/com/vlabs/wiimmfiapp/di/GameModules.kt
@@ -1,13 +1,20 @@
 package br.com.vlabs.wiimmfiapp.di
 
+import br.com.vlabs.data.repository.WiimmfiRepository
+import br.com.vlabs.domain.repository.GameRepository
 import br.com.vlabs.wiimmfiapp.game.stats.GameStatsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
+val repositoryModule = module {
+    single<GameRepository> { WiimmfiRepository() }
+}
+
 val viewModelModule = module {
-    viewModel { GameStatsViewModel() }
+    viewModel { GameStatsViewModel(get()) }
 }
 
 val appModules = listOf(
+    repositoryModule,
     viewModelModule
 )
