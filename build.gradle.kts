c71274ba41682ad7c42efc442e7567407d4b2d19
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2020-09-05 22:23:35 -0300

    Add safe args navigation

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:${Versions.gradle}")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.nav_version}")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
