215e10fd5649ccc25af5c99350f09bd05450ed81
Vilmar Bispo Filho <vilmarbf.ads@gmail.com>
2020-11-13 22:16:52 -0300

    Create delegate to ViewBinding

---
+++ app/src/main/java/br/com/vlabs/wiimmfiapp/common/viewbinding/ActivityViewBindingDelegate.kt
@@ -0,0 +1,27 @@
+package br.com.vlabs.wiimmfiapp.common.viewbinding
+
+import android.app.Activity
+import android.view.LayoutInflater
+import androidx.viewbinding.ViewBinding
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+inline fun <reified T : ViewBinding> Activity.viewBinding() = ActivityViewBindingDelegate(T::class.java)
+
+class ActivityViewBindingDelegate<T : ViewBinding>(private val bindingClass: Class<T>) :
+    ReadOnlyProperty<Activity, T> {
+
+    private var binding: T? = null
+
+    override fun getValue(thisRef: Activity, property: KProperty<*>): T {
+        binding?.let { return it }
+
+        val inflateMethod = bindingClass.getMethod("inflate", LayoutInflater::class.java)
+
+        val invokeLayout = inflateMethod.invoke(null, thisRef.layoutInflater) as T
+
+        thisRef.setContentView(invokeLayout.root)
+
+        return invokeLayout.also { this.binding = it }
+    }
+}
